6009f2209f875847fb51578929e0e32adcaa9e35
CyprianDavis <128586128+CyprianDavis@users.noreply.github.com>
2024-01-18 01:11:06 +0300

    added methods to the interface and class

--- src/data/structure/Stack.java
+++ src/data/structure/Stack.java
@@ -1,5 +1,39 @@
 package data.structure;
+/**
+ * Stack ADT Interface
+ * Collection of objects inserted and removed according LIFO principle
+ * that is Lasr in first out
+ * @author CYPRIAN DAVIS
+ *
+ * @param <T>
+ */
 
 public interface Stack<T> {
+	/**
+	 * Adds element e to the top of the stack
+	 * @param e
+	 */
+	public void push(T e);
+	/**
+	 * Removes and returns the top element from the stack (or null if stack is empty)
+	 * @return
+	 */
+	public T pop();
+	/**
+	 * Returns the top element from the stack 
+	 * without removing it (or null if stack is empty)
+	 * @return
+	 */
+	public T top();
+	/**
+	 * Returns the number of elements in the stack
+	 * @return
+	 */
+	public int size();
+	/**
+	 * Returns a boolean indicating whether the stack is empty
+	 * @return
+	 */
+	public boolean isEmpty();
 
 }
--- src/data/structure/StackArray.java
+++ src/data/structure/StackArray.java
@@ -6,6 +6,37 @@ package data.structure;
  * @param <T>
  */
 
-public class StackArray<T> {
+public class StackArray<T> implements Stack<T>{
+
+	@Override
+	public void push(T e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T top() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 
 }
